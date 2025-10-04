9d3f9f6f9a419e67eebcce99b9340b57a0d96b0e
AntonSkyrda <mr.skyrda@gmail.com>
2025-10-05 02:26:31 +0300

    done hw-7 1st task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
+import {UseFetch} from "./hooks/UseFetch.tsx";
+import type {IUser} from "./models/IUser.ts";
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+    const users = UseFetch<IUser[]>(`${import.meta.env.VITE_API_JSONPLACEHOLDER_URL}users`, []);
+    console.log(users);
+    return (
+        <div>
+            {
+                users && users.map((user: IUser) => (
+                    <div key={user.id}>
+                        User Name: {user.username}
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default App
